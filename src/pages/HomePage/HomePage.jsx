cbb629c38966a7465e46df3f2ebd160b3af9b98a
Daniil <daniil424342@gmail.com>
2025-05-11 23:15:36 +0500

    question card list

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import { QuestionCard } from "../../components/QuestionCard";
 import { API_URL } from "../../constans";
+import { QuestionCardList } from "../../components/QuestionCardList";
 // import cls from "./HomePage.module.css";
 
 
@@ -24,13 +24,9 @@ export const HomePage = () => {
         getQuestions();
     }, []);
 
-    // getQuestions();
-
     return (
-    <> 
-        {questions.map((card, index) => {
-            return <QuestionCard card={card} key={index} />;
-        })}
+    <>
+        <QuestionCardList cards={questions} />
     </>
     );
 };
