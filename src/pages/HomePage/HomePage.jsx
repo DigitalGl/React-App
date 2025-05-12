197cddf0d08a921b19a640770c76a7dd4185f97c
Daniil <daniil424342@gmail.com>
2025-05-12 16:44:37 +0500

    input, memo

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -8,6 +8,7 @@ import { useFetch } from "../../hooks/useFetch";
 
 export const HomePage = () => {
     const [questions, setQuestions] = useState([]);
+    const [serchValue, setSerchValue] = useState("");
 
     const [getQuestions, isLoading, error] = useFetch(async (url) => {
         const response = await fetch(`${API_URL}/${url}`);
@@ -21,8 +22,14 @@ export const HomePage = () => {
         getQuestions("react");
     }, []);
 
+    const onSearchChangeHandler = (e) => {
+        setSerchValue(e.target.value);
+    }
+
     return (
     <>
+        <input type="text" value={serchValue} onChange={onSearchChangeHandler} />
+
         {isLoading && <Loader />}
         {error && <p>{error}</p>}
         <QuestionCardList cards={questions} />
