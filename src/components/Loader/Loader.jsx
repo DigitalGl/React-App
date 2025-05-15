e295b7d732a1a3058d66f307d79121501b468154
Daniil <daniil424342@gmail.com>
2025-05-15 17:29:23 +0500

    question page, small loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,6 +2,10 @@ import cls from './Loader.module.css';
 
 export const Loader = () => {
     return <div className={cls.backdrop}>
-        <div className={cls.loader}></div>
+        <span className={cls.loader}></span>
     </div>
+};
+
+export const SmallLoader = () => {
+    return <span className={cls.smallLoader}></span>
 };
