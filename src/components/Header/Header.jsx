42c166452e01c2c1af2a0ca31d7871f11096b50f
Daniil <daniil424342@gmail.com>
2025-05-18 19:25:54 +0500

    auth provider, protected route

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,19 @@
 import { useNavigate } from "react-router-dom";
 import ReactLogo from "../../assets/react.svg";
 import { Button } from "../Button";
+import { useAuth } from "../../hooks/useAuth";
+import { AUTH_STORAGE } from "../../constants";
 import cls from "./Header.module.css";
 
 export const Header = () => {
   const navigate = useNavigate();
+  const { isAuth, setIsAuth} = useAuth();
+
+  const LoginHandler = () => {
+    localStorage.setItem(AUTH_STORAGE, !isAuth);
+    setIsAuth(!isAuth);
+  };
+
   return (
     <header className={cls.header}>
       <p onClick={() => navigate("/")}>
@@ -13,10 +22,10 @@ export const Header = () => {
       </p>
 
       <div className={cls.HeaderButtons}>
-        <Button onClick={() => navigate("/addquestion")} isActive>
+        {isAuth && <Button onClick={() => navigate("/addquestion")} isActive>
           Add
-        </Button>
-        <Button>login</Button>
+        </Button>}
+        <Button onClick={LoginHandler} isActive={!isAuth}>{isAuth ? "Logout" : "Login"}</Button>
       </div>
     </header>
   );
